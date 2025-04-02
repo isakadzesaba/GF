b7f6dacd9ab0fbd7ce6588203b6ce70a411277c8
isakadzesaba <sabaisakadze99@gmail.com>
2025-04-02 20:56:49 +0400

    commit

--- Heart/ajax.js
+++ Heart/ajax.js
@@ -1,16 +1,3 @@
-/**
- * dat-gui JavaScript Controller Library
- * http://code.google.com/p/dat-gui
- *
- * Copyright 2011 Data Arts Team, Google Creative Lab
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
@@ -2505,4 +2492,3 @@ var index = {
 return index;
 
 })));
-//# sourceMappingURL=dat.gui.js.map
